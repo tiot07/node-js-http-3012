011478c0c0ff27ced66ef1b2ce14d0643282144d
tiot07 <tiot7155@gmail.com>
2019-01-02 01:07:48 +0900

    同期

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 'use strict';
 const http = require('http');
 const server = http.createServer((req, res) => {
-    res.writeHead(200, {
+    	console.info('['+new Date()+']Requested by'+req.connection.remoteAddress);
+	res.writeHead(200, {
         'Content-Type': 'text/plain; charset=utf-8'
     });
     res.write(req.headers['user-agent']);
     res.end();
-});
+}).on('error',(e)=>{
+	console.error('['+new Date()+']Server Error',e);
+}).on;
 const port = 8000;
 server.listen(port, () => {
     console.log('Listening on ' + port);
